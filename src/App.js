945c853db2992364c6996788b2e9bb5916856931
Seonghee Lee <sl994@cornell.edu>
2022-03-07 22:20:42 -0500

    create bars

--- src/App.js
+++ src/App.js
@@ -19,14 +19,35 @@ function App() {
       console.log(data)
       
       d3.select('#pgraphs').selectAll('p').data(data).enter().append('p').text(dt => dt.Commodity + ": " + dt.Kilo)
+
+      
+      const getMax = () => { 
+        let max = 0
+        data.forEach((dt) => {
+            max = dt.Kilo
+        })
+        return max
+      }
+
+      // Food Waste Average Bar display
+      // Before bar animation (might have to delete later)
+      d3.select('#BarChart').selectAll('div').data(data) 
+      .enter().append('div').classed('bar', true).style('height', `${(getMax()*20)+150 }px`)
+  
+      // After bar animation 
+      d3.select('#BarChart').selectAll('.bar')
+      .transition().duration(1000).style('height', bar => `${(bar.Kilo*20)+150}px`)
+        .style('width', '80px').style('margin-right', '10px').delay(300) // Fix their width and margin
+
     });
     return () => undefined;
-
   }, [])
   
   return (
     <div className="App">
       <body>
+        <h3>Bar Chart of Food Waste Weight Average (Kilo)</h3>
+        <div id="BarChart"></div>
         <div id="pgraphs"></div>
       </body>
     </div>
